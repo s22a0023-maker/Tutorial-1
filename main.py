305d4ae9d3fe70999d8d632a29eb5353eba98528
s22a0023-maker <s22a0023@siswa.umk.edu.my>
2025-10-02 19:18:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import streamlit as st
 
-st.title("YUSRI BIN RAZALI - Job Applications")
-st.header("Contact Information: ")
+st.title("YUSRI BIN RAZALI - Internship Applications")
+st.header("Contact Information")
 
 st.write("Email: yuyie047@gmail.com")
 st.write("Phone: +60 19651 9305")
 st.write("LinkedIn: https://www.linkedin.com/in/yusri-razali-b659502a8/") 
 
 st.header("Education")
-st.write("Bachelor of Information Technology with Honors (Artificial Intelligence Track), Universiti Malaysia Kelantan (UMK), Year 4") 
+st.write("Bachelor of Information Technology with Honors - Artificial Intelligence Track, Universiti Malaysia Kelantan (UMK), Year 4") 
+
+st.header("Work Experience")
+st.write("President of Club, IT Club, 24/25")
+st.write("-Responsible for leading the team towards greater excellence and proactiveness.")
+st.write("-Collaborate with external clubs to manage projects and brainstorm ideas") 
+
+st.header("Skills") 
+st.write("-Artificial Intellingence (AI)/Machine Learning (ML) Developement")
+st.write("-Website/Apps Developement and Designer")
+st.write("-CyberSecurity Specialist/Ethical Hacking") 
+
+st.header("Project")
+st.write("Smart Macaque Detection Live Tracking Cage Monitoring Through CCTV Applications: To develop a real-time monkey detection system using the YOLOv11 object detection algorithm, optimized for deployment on video surveillance feeds in urban, agricultural, and forest-edge environments.")
