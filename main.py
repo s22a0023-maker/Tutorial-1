8d852d8726de67ba0a9330190a025fcc6f607ab7
s22a0023-maker <s22a0023@siswa.umk.edu.my>
2025-10-02 21:52:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ col1, col2 = st.columns([1, 3])
 
 with col1:
     # Use local image (must be in same folder as app.py)
-    if os.path.exists("3258247.jpg"):
-        st.image("3258247.jpg", caption="YUSRI BIN RAZALI", width=180)
+    if os.path.exists("profile.jpg"):
+        st.image("profile.jpg", caption="YUSRI BIN RAZALI", width=180)
     else:
         st.warning("profile.jpg not found. Please place it in the same folder as this app.")
 
