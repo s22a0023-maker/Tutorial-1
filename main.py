77cde32d9171f2134d2d425dfd52d7b6068da4a1
s22a0023-maker <s22a0023@siswa.umk.edu.my>
2025-10-02 21:39:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,32 +7,32 @@ col1, col2 = st.columns([1, 3])
 with col1:
     # Use local image (must be in same folder as app.py)
     if os.path.exists("3258247.jpg"):
-        st.image("3258247.jpg", caption="Yusri Bin Razali", width=180)
+        st.image("3258247.jpg", caption="YUSRI BIN RAZALI", width=180)
     else:
         st.warning("profile.jpg not found. Please place it in the same folder as this app.")
 
 with col2:
     st.title("YUSRI BIN RAZALI")
-    st.markdown("**Internship Applications**")
+    st.markdown("**Internship Resume Page**")
     st.markdown(""" 
-    - **Email:** yuyie047@gmail.com
-    - **Phone:** +60 19651 9305
-    - **LinkedIn:** (https://www.linkedin.com/in/yusri-razali-b659502a8/) 
+    📧 **Email:** yuyie047@gmail.com
+    📞 **Phone:** +60 19651 9305
+    🔗 **LinkedIn:** (https://www.linkedin.com/in/yusri-razali-b659502a8/) 
     """)
 
 #Education
 st.header("🎓 Education")
 st.markdown("""
-**Bachelor of Information Technology with Honors (AI Track)**  
+**Bachelor of Information Technology with Honors - AI Track,**
 Universiti Malaysia Kelantan (UMK), Year 4
 """)
 
 #Experience
 st.header("💼 Work Experience")
-st.subheader("President of IT Club (2024/25)")
+st.subheader("Promoter, PETRONAS, 2023")
 st.markdown("""
-- Led the team towards greater excellence and proactiveness  
-- Collaborated with external clubs to manage projects and brainstorm ideas
+- Get a customer support for Setel apps to refill the fuel easily.  
+- Provide the benefits and advantages of using the Setel app.
 """)
 
 #Skills
@@ -49,4 +49,24 @@ with col4:
 
 #Project
 st.header("Projects")
-st.write("Smart Macaque Detection Live Tracking Cage Monitoring Through CCTV Applications: To develop a real-time monkey detection system using the YOLOv11 object detection algorithm, optimized for deployment on video surveillance feeds in urban, agricultural, and forest-edge environments.")
+st.subheader("Smart Macaque Detection & Live Tracking")
+st.markdown("""
+Developed a **real-time monkey detection system** using **YOLOv11**,  
+optimized for deployment on **CCTV video surveillance feeds** in urban, agricultural, and forest-edge environments.
+""")
+
+#Achievements 
+st.header("Achievements") 
+st.markdown("**3rd Place in Trailblazer Cup 2025**")
+st.markdown("**1st place in Lawn Ball Men's Category of KRISMA 2025**")
+st.markdown("**Dean's Award: Semester 1, 2, and 5**")
+
+#Club and Societies 
+st.header("Activities and Societies")
+st.subheader("**Information Technology Club (SST) 24/25**")
+st.markdown("**Position:** President")
+st.subheader("**Acedemic Ambassador of FSDK 24/25**")
+st.subheader("**Member of the Student Conference Board 24/25**")
+st.subheader("**Genius Madani @ SMK Machang 2025**")
+st.markdown("**Position:** Chief Facilitator") 
+
