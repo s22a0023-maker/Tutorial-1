335b356494e446f3dacf0c381431200f497adfa8
s22a0023-maker <s22a0023@siswa.umk.edu.my>
2025-10-02 18:58:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("Your Name's Resume")
-st.header("Contact Information")
+st.title("YUSRI BIN RAZALI - Job Applications")
+st.header("Contact Information: ")
 
+st.write("Email: yuyie047@gmail.com")
+st.write("Phone: +60 19651 9305")
+st.write("LinkedIn: https://www.linkedin.com/in/yusri-razali-b659502a8/") 
 
+st.header("Education")
+st.write("Bachelor of Information Technology with Honors (Artificial Intelligence Track), Universiti Malaysia Kelantan (UMK), Year 4") 
