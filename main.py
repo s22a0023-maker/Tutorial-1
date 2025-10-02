470473fcc49421891649b96ea0dbfbb86c9416d4
s22a0023-maker <s22a0023@siswa.umk.edu.my>
2025-10-02 20:02:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,32 @@ st.header("Contact Information")
 st.markdown(""" 
 - **Email:** yuyie047@gmail.com
 - **Phone:** +60 19651 9305
-- **"LinkedIn:** (https://www.linkedin.com/in/yusri-razali-b659502a8/) 
+- **LinkedIn:** (https://www.linkedin.com/in/yusri-razali-b659502a8/) 
 """)
 
-st.header("Education")
-st.write("Bachelor of Information Technology with Honors - Artificial Intelligence Track, Universiti Malaysia Kelantan (UMK), Year 4") 
+st.header("🎓 Education")
+st.markdown("""
+**Bachelor of Information Technology with Honors (AI Track)**  
+Universiti Malaysia Kelantan (UMK), Year 4
+""")
 
-st.header("Work Experience")
-st.write("President of Club, IT Club, 24/25")
-st.write("-Responsible for leading the team towards greater excellence and proactiveness.")
-st.write("-Collaborate with external clubs to manage projects and brainstorm ideas") 
+st.header("💼 Work Experience")
+st.subheader("President of IT Club (2024/25)")
+st.markdown("""
+- Led the team towards greater excellence and proactiveness  
+- Collaborated with external clubs to manage projects and brainstorm ideas
+""")
 
-st.header("Skills") 
-st.write("-Artificial Intellingence (AI)/Machine Learning (ML) Developement")
-st.write("-Website/Apps Developement and Designer")
-st.write("-CyberSecurity Specialist/Ethical Hacking") 
+st.header("🛠 Skills")
+col3, col4 = st.columns(2)
+
+with col3:
+    st.markdown("- Artificial Intelligence / Machine Learning Development")  
+    st.markdown("- Website & Mobile App Development")  
+
+with col4:
+    st.markdown("- Cybersecurity / Ethical Hacking")  
+    st.markdown("- UI/UX Design & Prototyping") 
 
 st.header("Project")
 st.write("Smart Macaque Detection Live Tracking Cage Monitoring Through CCTV Applications: To develop a real-time monkey detection system using the YOLOv11 object detection algorithm, optimized for deployment on video surveillance feeds in urban, agricultural, and forest-edge environments.")
